4c08154f8d203a31404cc08a667f7741858327dc
Mubashir Dollie <mubashirdollie65@gmail.com>
2024-12-05 15:28:37 +0200

    Update Main.java

--- SolveForEarth/src/main/java/org/example/Main.java
+++ SolveForEarth/src/main/java/org/example/Main.java
@@ -2,8 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Lets solve for Earth...");
-        System.out.println("North + East + South + West = Earth\n");
+        System.out.println("Lets solve for the following eqxn North + East + South + West = Earth\n");
 
         CryptarithmSolver solver = new CryptarithmSolver();
         solver.solve();
